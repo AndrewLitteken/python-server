cb12b4ea46c5f138c504d05b263a43991ae4d617
Andrew Litteken <andrew.litteken@gmail.com>
2016-10-01 21:12:49 -0400

    pefully a fix

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import json
 import os
 import HttpResponse
@@ -13,33 +14,29 @@ listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 while True:
     client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print request
+
+    while True:
+        request = client_connection.recv(1024)
+        print request
 
     '''with open("userAnalyticsPy.json") as json_file:
         d = json.load(json_file)
         print(d)'''
+        if request:
+            tradeoff_analytics = TradeoffAnalyticsV1(
+            username='af7839e5-5719-4357-9f2f-8b4607ba84d5',
+            password='IY2KB0panLuF')
 
-    tradeoff_analytics = TradeoffAnalyticsV1(
-        username='af7839e5-5719-4357-9f2f-8b4607ba84d5',
-        password='IY2KB0panLuF')
-
-    with open(join(dirname(__file__), 'userAnalyticsPy.json')) as data_file:
-        problem_data = json.load(data_file)
-    # print(json.dumps(tradeoff_analytics.dilemmas(problem_data), indent=2))
-    resolution = tradeoff_analytics.dilemmas(problem_data)
-    solutionNum = 0
-    while resolution["resolution"]["solutions"][solutionNum]["status"]!="FRONT":
-        solutionNum = solutionNum+1
-    firstChoice = resolution["resolution"]["solutions"][solutionNum]["solution_ref"]
-    index = int(firstChoice)-1
-    reccomend = resolution["problem"]["options"][index]["name"]
-    HttpResponse(reccomend)
-
-    http_response = """\
-HTTP/1.1 200 OK
-
-Hello, World!
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
+            with open(join(dirname(__file__), 'userAnalyticsPy.json')) as data_file:
+                problem_data = json.load(data_file)
+                # print(json.dumps(tradeoff_analytics.dilemmas(problem_data), indent=2))
+            resolution = tradeoff_analytics.dilemmas(problem_data)
+            solutionNum = 0
+            while resolution["resolution"]["solutions"][solutionNum]["status"]!="FRONT":
+                solutionNum = solutionNum+1
+            firstChoice = resolution["resolution"]["solutions"][solutionNum]["solution_ref"]
+            index = int(firstChoice)-1
+            reccomend = resolution["problem"]["options"][index]["name"]
+            client_connection.sendall(reccomend)
+            else:
+                break
